cf91f14e88642b495474a32005ba3f03c82b5ce7
Allen Gueco <allen.gueco1@gmail.com>
2024-07-23 17:54:08 -0400

    Add home/.config/wezterm/wezterm.lua

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = 'Catppuccin Frappe'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+
+ return config
