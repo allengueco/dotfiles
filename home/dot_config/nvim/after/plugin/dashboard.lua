4b71debc19b83d5a316332d432091e6cde37d606
Allen Gueco <allen.gueco1@gmail.com>
2024-07-27 17:34:56 -0400

    Update home/.config/nvim/after/plugin/dashboard.lua

--- home/dot_config/nvim/after/plugin/dashboard.lua
+++ home/dot_config/nvim/after/plugin/dashboard.lua
@@ -4,25 +4,31 @@ require('dashboard').setup {
         shortcut = {
             -- { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
             -- {
-            --     icon = ' ',
-            --     icon_hl = '@variable',
-            --     desc = 'Files',
-            --     group = 'Label',
-            --     action = 'Telescope find_files',
-            --     key = 'f',
-            -- },
-            {
-                desc = ' Apps',
-                group = 'DiagnosticHint',
-                action = 'Telescope app',
-                key = 'a',
+                --     icon = ' ',
+                --     icon_hl = '@variable',
+                --     desc = 'Files',
+                --     group = 'Label',
+                --     action = 'Telescope find_files',
+                --     key = 'f',
+                -- },
+                {
+                    desc = ' Apps',
+                    group = 'DiagnosticHint',
+                    action = 'Telescope app',
+                    key = 'a',
+                },
+                {
+                    desc = ' Chezmoi',
+                    group = 'DiagnosticHint',
+                    action = 'Telescope chezmoi',
+                    key = 'c',
+                },
+                {
+                    desc = ' dotfiles',
+                    group = 'Number',
+                    action = 'Telescope dotfiles',
+                    key = 'd',
+                },
             },
-            {
-                desc = ' dotfiles',
-                group = 'Number',
-                action = 'Telescope dotfiles',
-                key = 'd',
-            },
-        },
+        }
     }
-}
