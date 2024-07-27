6697b0619b3e49b3a8c179613b9e89c099a0cedf
Allen Gueco <allen.gueco1@gmail.com>
2024-07-27 17:37:38 -0400

    Update home/.config/nvim/after/plugin/telescope.lua

--- home/dot_config/nvim/after/plugin/telescope.lua
+++ home/dot_config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,4 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
@@ -5,3 +6,6 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("rg > ") });
 end)
+
+telescope.load_extension('chezmoi')
+
