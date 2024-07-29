e8a65d0ee4b194abe9ee5169c211cc96052fb6ef
Allen Gueco <allen.gueco1@gmail.com>
2024-07-28 23:51:13 -0400

    Update home/.config/nvim/lua/allengueco/packer.lua

--- home/dot_config/nvim/lua/allengueco/packer.lua
+++ home/dot_config/nvim/lua/allengueco/packer.lua
@@ -116,7 +116,11 @@ return require('packer').startup(function(use)
         'stevearc/conform.nvim',
         event = { 'BufReadPre', 'BufNewFile' },
         config = function()
-            require('conform').setup ()
+            require('conform').setup {
+                formatters_by_ft = {
+                    markdown = { 'prettier' }
+                }
+            }
         end,
     }
 end)
