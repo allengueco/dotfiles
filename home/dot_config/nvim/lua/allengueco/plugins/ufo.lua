8442988a49657d44da00466f1f4d8579a99118ec
Allen Gueco <allen.gueco1@gmail.com>
2024-08-24 15:07:45 -0400

    Update home/.config/nvim/lua/allengueco/plugins/ufo.lua

--- home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
@@ -4,9 +4,9 @@ return {
         'kevinhwang91/promise-async'
     },
     init = function()
-        vim.o.foldcolumn = '1',
-        vim.o.foldlevel = 99,
-        vim.o.foldlevelstart = 99,
+        vim.o.foldcolumn = '1'
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
         vim.o.foldenable = true
     end,
     opts = {
