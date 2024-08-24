469a7eb96cbeeceef0b36b001824416f16519940
Allen Gueco <allen.gueco1@gmail.com>
2024-08-24 18:33:51 -0400

    Update home/.config/nvim/lua/allengueco/lazy.lua

--- home/dot_config/nvim/lua/allengueco/lazy.lua
+++ home/dot_config/nvim/lua/allengueco/lazy.lua
@@ -20,7 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- import your plugins
-        { 'JavaHello/spring-boot.nvim', ft = 'java' },
         { 'stevearc/dressing.nvim', opts = {} },
         { 'tpope/vim-fugitive' },
         { 'nvim-tree/nvim-web-devicons' },
