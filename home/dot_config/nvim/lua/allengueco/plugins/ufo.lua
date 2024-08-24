59f1afa794aa0c6d4d04ac0b7339c2c6adaff35b
Allen Gueco <allen.gueco1@gmail.com>
2024-08-24 15:06:24 -0400

    Update home/.config/nvim/lua/allengueco/plugins/ufo.lua

--- home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
@@ -7,7 +7,7 @@ return {
         vim.o.foldcolumn = '1',
         vim.o.foldlevel = 99,
         vim.o.foldlevelstart = 99,
-        vim.o.foldenable = true,
+        vim.o.foldenable = true
     end,
     opts = {
         provider_selector = function(bufnr, filetype, buftype)
