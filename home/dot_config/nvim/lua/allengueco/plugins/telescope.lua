5610eee9d3446de8d50995919de17e8696a65e14
Allen Gueco <allen.gueco1@gmail.com>
2024-08-24 17:50:38 -0400

    Update home/.config/nvim/lua/allengueco/plugins/telescope.lua

--- home/dot_config/nvim/lua/allengueco/plugins/telescope.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/telescope.lua
@@ -7,16 +7,13 @@ return {
     dependencies = {
         { 'nvim-lua/plenary.nvim' },
         { 'nvim-telescope/telescope-ui-select.nvim' },
-        { 'jonarrien/telescope-cmdline.nvim' }
     },
     keys = {
         { '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>" },
         { '<leader>fg', "<cmd>lua require('telescope.builtin').git_files()<CR>"},
         { '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>"},
-        { ':', '<cmd> Telescope cmdline<cr>', noremap = true, desc = 'Cmdline' }
     },
     config = function ()
        require('telescope').load_extension('ui-select')
-       require('telescope').load_extension('cmdline')
     end
 }
