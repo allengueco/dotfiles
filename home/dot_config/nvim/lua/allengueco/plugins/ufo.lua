f479cf7f8b0eeab62ab8e58568d4f041e2fe6536
Allen Gueco <allen.gueco1@gmail.com>
2024-08-24 19:52:49 -0400

    Update home/.config/nvim/lua/allengueco/plugins/ufo.lua

--- home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
@@ -4,29 +4,9 @@ return {
         'kevinhwang91/promise-async'
     },
     init = function()
-        vim.o.foldcolumn = '1'
-        vim.o.foldlevel = 99
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
-    end,
-    config = function()
-        local lsp_zero = require('lsp-zero')
-
-        local lsp_capabilities = vim.tbl_deep_extend(
-            'force',
-            require('cmp_nvim_lsp').default_capabilities(),
-            {
-                textDocument = {
-                    foldingRange = {
-                        dynamicRegistration = false,
-                        lineFoldingOnly = true
-                    }
-                }
-            }
-        )
-
-        lsp_zero.extend_lspconfig({
-            capabilities = lsp_capabilities,
-        })
+        vim.o.foldcolumn = '1',
+        vim.o.foldlevel = 99,
+        vim.o.foldlevelstart = 99,
+        vim.o.foldenable = true,
     end,
 }
