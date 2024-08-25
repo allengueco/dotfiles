33462e42aa06a940fefa44d544b93b1907c451a1
Allen Gueco <allen.gueco1@gmail.com>
2024-08-25 04:02:54 -0400

    Update home/.config/nvim/lua/allengueco/lazy.lua

--- home/dot_config/nvim/lua/allengueco/lazy.lua
+++ home/dot_config/nvim/lua/allengueco/lazy.lua
@@ -7,7 +7,7 @@ if not uv.fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -20,12 +20,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- import your plugins
-        { 'stevearc/dressing.nvim', opts = {} },
+        { 'stevearc/dressing.nvim',         opts = {} },
         { 'tpope/vim-fugitive' },
         { 'nvim-tree/nvim-web-devicons' },
-        { 'saecki/crates.nvim', event = 'BufRead Cargo.toml', tag = 'stable' },
+        { 'saecki/crates.nvim',             event = 'BufRead Cargo.toml', tag = 'stable' },
         { 'nvim-neotest/nvim-nio' },
-        { 'windwp/nvim-autopairs', event = 'InsertEnter', config = true },
+        { 'lukas-reineke/indent-blankline', main = 'ibl',                 opts = {} },
+        { 'windwp/nvim-autopairs',          event = 'InsertEnter',        config = true },
         { import = "allengueco.plugins" },
     },
     -- Configure any other settings here. See the documentation for more details.
