0b1c7271402ea2bfc179c4a40489c6eebf84c568
Allen Gueco <allen.gueco1@gmail.com>
2024-08-25 03:53:04 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -114,7 +114,7 @@ return {
                         require('lspconfig')[server_name].setup({})
                     end,
                     -- Configured with jdtls.lua
-                    jdtls = lsp_zero.noop,
+                    jdtls = function() end,
                     lua_ls = function()
                         require('lspconfig').lua_ls.setup {
                             on_init = function(client)
