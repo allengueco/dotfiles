adb1865e174cf4eab9e0455274045e3559605b64
Allen Gueco <allen.gueco1@gmail.com>
2024-08-30 17:39:25 -0400

    Update home/.config/nvim/lua/allengueco/plugins/nvim-tree.lua

--- home/dot_config/nvim/lua/allengueco/plugins/nvim-tree.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/nvim-tree.lua
@@ -5,6 +5,23 @@ return {
     init = function()
         vim.g.barbar_auto_setup = false
     end,
+    config = function()
+        local function open_nvim_tree(data)
+            local real_file = vim.fn.filereadable(data.file) == 1
+
+            local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+
+            if not real_file and not no_name then
+                return
+            end
+
+            require('nvim-tree.api').tree.toggle({ focuse = false })
+
+            vim.api.nvim_exec_autocmds('BufWinEnter', { buffer = require('nvim-tree.view').get_bufnr() })
+        end
+
+        vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+    end,
     dependencies = {
         'nvim-tree/nvim-web-devicons',
     },
