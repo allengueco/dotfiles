e0a7ff4dddb7558ad2dc02ae22331482354e4894
Allen Gueco <allen.gueco1@gmail.com>
2024-08-30 17:21:58 -0400

    Update home/.config/nvim/lua/allengueco/init.lua

--- home/dot_config/nvim/lua/allengueco/init.lua
+++ home/dot_config/nvim/lua/allengueco/init.lua
@@ -1,4 +1,6 @@
+-- set is first so we can set these first:
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 require("allengueco.set")
 require("allengueco.remap")
 require("allengueco.lazy")
-
