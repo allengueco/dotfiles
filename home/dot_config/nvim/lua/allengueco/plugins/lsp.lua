4cd48bcd75daba17382f81ff17b0b07a7dc1bdf9
Allen Gueco <allen.gueco1@gmail.com>
2024-09-14 18:07:52 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -116,26 +116,26 @@ return {
                             }
                         }
                     end,
-                    angularls = function()
-                        local angularls = require('mason-registry').get_package('angular-language-server')
-                            :get_install_path()
-
-                        local cmd = {
-                            'ngserver',
-                            '--stdio',
-                            '--tsProbeLocations',
-                            table.concat({ angularls, vim.uv.cwd() }, ','),
-                            '--ngProbeLocations',
-                            table.concat({ angularls .. '/node_modules/@angular/language-server', vim.uv.cwd() }, ','),
-                        }
-
-                        require('lspconfig').angularls.setup {
-                            cmd = cmd,
-                            on_new_config = function(new_config, new_root_dir)
-                                new_config.cmd = cmd
-                            end,
-                        }
-                    end,
+                    -- angularls = function()
+                    --     local angularls = require('mason-registry').get_package('angular-language-server')
+                    --         :get_install_path()
+                    --
+                    --     local cmd = {
+                    --         'ngserver',
+                    --         '--stdio',
+                    --         '--tsProbeLocations',
+                    --         table.concat({ angularls, vim.uv.cwd() }, ','),
+                    --         '--ngProbeLocations',
+                    --         table.concat({ angularls .. '/node_modules/@angular/language-server', vim.uv.cwd() }, ','),
+                    --     }
+                    --
+                    --     require('lspconfig').angularls.setup {
+                    --         cmd = cmd,
+                    --         on_new_config = function(new_config, new_root_dir)
+                    --             new_config.cmd = cmd
+                    --         end,
+                    --     }
+                    -- end,
                 }
             })
         end
