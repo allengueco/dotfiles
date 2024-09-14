65cb603588c1fde76b1f6bb12a96ec6d36834034
Allen Gueco <allen.gueco1@gmail.com>
2024-09-14 15:30:48 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -115,7 +115,27 @@ return {
                                 "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "quarto", "rmd", "context", "text"
                             }
                         }
-                    end
+                    end,
+                    angularls = function()
+                        local angularls = require('mason-registry').get_package('angular-language-server')
+                            :get_install_path()
+
+                        local ngserver = angularls .. '/node_modules/@angular/language-server/bin/ngserver'
+                        local cmd = {
+                            ngserver,
+                            '--tsProbeLocations',
+                            table.concat({ angularls, vim.uv.cwd() }, ','),
+                            '--ngProbeLocations',
+                            table.concat({ angularls .. '/node_modules/@angular/language-server', vim.uv.cwd() }, ','),
+                        }
+
+                        require('lspconfig').angularls.setup {
+                            cmd = cmd,
+                            on_new_config = function(new_config, new_root_dir)
+                                new_config.cmd = cmd
+                            end,
+                        }
+                    end,
                 }
             })
         end
