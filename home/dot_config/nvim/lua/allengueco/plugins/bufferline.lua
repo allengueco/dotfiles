0e2e7f9ec59149bd7bccc5e30de3a533a94c38e6
Allen Gueco <allen.gueco1@gmail.com>
2024-09-16 20:12:45 -0400

    Update home/.config/nvim/lua/allengueco/plugins/bufferline.lua

--- home/dot_config/nvim/lua/allengueco/plugins/bufferline.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/bufferline.lua
@@ -21,5 +21,16 @@ return {
                 }
             }
         },
+    },
+    keys = {
+        { "<Alt>1", "<Cmd>BufferLineGoToBuffer 1<CR>" }
+        { "<Alt>2", "<Cmd>BufferLineGoToBuffer 2<CR>" }
+        { "<Alt>3", "<Cmd>BufferLineGoToBuffer 3<CR>" }
+        { "<Alt>4", "<Cmd>BufferLineGoToBuffer 4<CR>" }
+        { "<Alt>5", "<Cmd>BufferLineGoToBuffer 5<CR>" }
+        { "<Alt>6", "<Cmd>BufferLineGoToBuffer 6<CR>" }
+        { "<Alt>7", "<Cmd>BufferLineGoToBuffer 7<CR>" }
+        { "<Alt>8", "<Cmd>BufferLineGoToBuffer 8<CR>" }
+        { "<Alt>9", "<Cmd>BufferLineGoToBuffer 9<CR>" }
     }
 }
