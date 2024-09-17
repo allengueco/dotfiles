f55d53e744522602b7e5a89c4ec357c4ca61fddb
Allen Gueco <allen.gueco1@gmail.com>
2024-09-16 20:38:17 -0400

    Update home/.config/nvim/lua/allengueco/remap.lua

--- home/dot_config/nvim/lua/allengueco/remap.lua
+++ home/dot_config/nvim/lua/allengueco/remap.lua
@@ -5,4 +5,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "<A-c>", "bdelete! %")
