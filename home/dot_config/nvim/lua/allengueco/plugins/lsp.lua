b306b4f548cf95635339ab7ca374a89255a62bc8
Allen Gueco <allen.gueco1@gmail.com>
2024-09-18 19:49:47 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
             }
         }
     },
-    { 'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendy-snippets' } },
+    { 'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets' } },
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
