8e29e92c5a92b9fff6a9bcfe6855e73faa97a12f
Allen Gueco <allen.gueco1@gmail.com>
2024-09-25 02:21:29 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -157,5 +157,26 @@ return {
                 }
             })
         end
-    }
+    },
+    {
+        'stevearc/conform.nvim',
+        event = { 'BufWritePre' },
+        cmd = {'ConformInfo'},
+        keys = {
+            {
+                '<Leader>gq', function ()
+                    require('conform').format({ async = true })
+                end,
+                desc = 'Format Buffer'
+            }
+        },
+        opts = {
+            formatters_by_ft = {
+            },
+            default_format_opts = {
+                lsp_format = 'fallback'
+            },
+            format_on_save = { timeout_ms = 500 },
+        },
+    },
 }
