2f8b028d99147cf819dc183e63757e6fd7dfc656
Allen Gueco <allen.gueco1@gmail.com>
2024-10-14 17:25:35 -0400

    Update home/.config/nvim/lua/allengueco/plugins/ufo.lua

--- home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/ufo.lua
@@ -1,6 +1,5 @@
 return {
     'kevinhwang91/nvim-ufo',
-    enabled = false,
     dependencies = {
         'kevinhwang91/promise-async'
     },
@@ -9,15 +8,13 @@ return {
         vim.o.foldlevel = 99
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-        vim.api.nvim_create_autocmd('Filetype', {
-            pattern = {
-                'NeogitStatus',
-            },
-            callback = function()
-                require('ufo').detach()
-                vim.opt_local.foldenable = false
-                vim.opt_local.foldcolumn = '0'
-            end
-        })
     end,
+    keys = {
+        {
+            'zR', "<Cmd>lua require('ufo').openAllFolds()<CR>"
+        },
+        {
+            'zM', "<Cmd>lua require('ufo').closeAllFolds()<CR>"
+        }
+    }
 }
