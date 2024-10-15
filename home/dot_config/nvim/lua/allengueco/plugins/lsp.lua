ee61f8ab6182b15950528da63895f12aef720f19
Allen Gueco <allen.gueco1@gmail.com>
2024-10-14 21:37:20 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -137,7 +137,7 @@ return {
 
             local lsp_capabilities = vim.tbl_deep_extend(
                 'force',
-                require('cmp_nvim_lsp').default_capabilities(),
+                vim.lsp.protocol.make_client_capabilities(),
                 {
                     textDocument = {
                         foldingRange = {
