dd9b20808f9480d398e3263ba0c124fd88993a80
Allen Gueco <allen.gueco1@gmail.com>
2024-10-26 19:21:54 -0400

    Update home/.config/nvim/lua/allengueco/plugins/catppuccin.lua

--- home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
@@ -3,33 +3,35 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+        require('catppuccin').setup(
+            {
+                integrations = {
+                    barbar = true,
+                    nvim_surround = true,
+                    blink_cmp = true,
+                    lsp_trouble = true,
+                    diffview = true,
+                    mason = true,
+                    indent_blankline = {
+                        colored_indent_levels = true,
+                    },
+                    neotest = true,
+                    native_lsp = {
+                        enabled = true,
+                        underlines = {
+                            errors = { "undercurl" },
+                            hints = { "undercurl" },
+                            warnings = { "undercurl" },
+                            information = { "undercurl" },
+                        },
+                    },
+                    which_key = true,
+                },
+                styles = {
+                    functions = { "italic" }
+                }
+            }
+        )
         vim.cmd.colorscheme "catppuccin-frappe"
     end,
-    opts = {
-        integrations = {
-            barbar = true,
-            nvim_surround = true,
-            blink_cmp = true,
-            lsp_trouble = true,
-            diffview = true,
-            mason = true,
-            indent_blankline = {
-                colored_indent_levels = true,
-            },
-            neotest = true,
-            native_lsp = {
-                enabled = true,
-                underlines = {
-                    errors = { "undercurl" },
-                    hints = { "undercurl" },
-                    warnings = { "undercurl" },
-                    information = { "undercurl" },
-                },
-            },
-            which_key = true,
-        },
-        styles = {
-            functions = { "italic" }
-        }
-    }
 }
