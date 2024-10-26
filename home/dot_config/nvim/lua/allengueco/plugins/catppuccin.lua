54c4cc83b1aa3a68386c7064845effe18d0d385a
Allen Gueco <allen.gueco1@gmail.com>
2024-10-26 19:04:31 -0400

    Update home/.config/nvim/lua/allengueco/plugins/catppuccin.lua

--- home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
@@ -9,8 +9,14 @@ return {
         integrations = {
             barbar = true,
             nvim_surround = true,
+            blink_cmp = true,
             lsp_trouble = true,
+            diffview = true,
             mason = true,
+            indent_blankline = {
+                colored_indent_levels = true,
+            },
+            neotest = true,
             native_lsp = {
                 enabled = true,
                 underlines = {
