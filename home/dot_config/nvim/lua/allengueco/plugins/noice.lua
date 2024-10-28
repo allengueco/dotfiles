040e1ff55377d811138d22d59c8a9b0e17aca97a
Allen Gueco <allen.gueco1@gmail.com>
2024-10-28 05:43:48 -0400

    Update home/.config/nvim/lua/allengueco/plugins/noice.lua

--- home/dot_config/nvim/lua/allengueco/plugins/noice.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/noice.lua
@@ -5,15 +5,11 @@ return {
 		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
 		opts = {
 			lsp = {
-				signature = {
-					enabled = false,
-				},
-				hover = {
-					enabled = false,
-				},
+
 			},
 			presets = {
 				long_message_to_split = true,
+                command_palette = true,
 				inc_rename = false,
 				lsp_doc_border = false,
 			},
@@ -28,36 +24,6 @@ return {
 						end,
 					},
 					opts = { skip = true },
-				},
-			},
-			views = {
-				cmdline_popup = {
-					position = {
-						row = 5,
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = "auto",
-					},
-				},
-				popupmenu = {
-					relative = "editor",
-					position = {
-						row = 8,
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = 10,
-					},
-					border = {
-						style = "rounded",
-						padding = { 0, 1 },
-					},
-					win_options = {
-						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-					},
 				},
 			},
 		},
