15efa61e35800aa645a821ba52e7bc03a3e06266
Allen Gueco <allen.gueco1@gmail.com>
2024-10-28 03:49:35 -0400

    Update home/.config/nvim/lua/allengueco/plugins/noice.lua

--- home/dot_config/nvim/lua/allengueco/plugins/noice.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/noice.lua
@@ -3,6 +3,9 @@ return {
 	event = "VeryLazy",
 	dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
 	opts = {
+        cmdline = {
+            
+        },
 		lsp = {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -16,11 +19,8 @@ return {
 			},
 		},
 		presets = {
-			bottom_search = true,
-			command_palette = true,
 			long_message_to_split = true,
 			inc_rename = false,
-			lsp_doc_border = true,
 		},
 	},
 }
