ca3d24d65a897ec967bc3f6ced41607129a29bb2
Allen Gueco <allen.gueco1@gmail.com>
2024-11-01 16:06:20 -0400

    Update home/.config/nvim/lua/allengueco/plugins/rustacean.lua

--- home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
@@ -2,4 +2,12 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^5",
 	lazy = false,
+	init = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+		vim.keymap.set("n", "K", function()
+			vim.cmd.RustLsp({ "hover", "action" })
+		end,
+        { silent = true, buffer = bufnr }
+        )
+	end,
 }
