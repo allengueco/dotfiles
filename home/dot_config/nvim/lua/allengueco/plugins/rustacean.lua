348826beb392d035980720c571df499be82450aa
Allen Gueco <allen.gueco1@gmail.com>
2024-11-01 15:49:24 -0400

    Update home/.config/nvim/lua/allengueco/plugins/rustacean.lua

--- home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
@@ -7,6 +7,18 @@ return {
 		vim.g.rustaceanvim = {
 			server = {
 				capabilities = require("lsp-zero").get_capabilities(),
+				on_attach = function(client, bufnr)
+					local bufnr = vim.api.nvim_get_current_buf()
+
+					vim.keymap.set("n", "<F4>", function()
+						vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+						-- or vim.lsp.buf.codeAction() if you don't want grouping.
+					end, { silent = true, buffer = bufnr })
+
+					vim.keymap.set("n", "K", function()
+						vim.cmd.RustLsp({ "hover", "action" })
+					end, { silent = true, buffer = bufnr })
+				end,
 			},
 		}
 	end,
