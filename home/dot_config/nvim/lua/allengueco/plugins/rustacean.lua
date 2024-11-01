3b29fed0b5936dfd81c7f3b1dba2448c6bacc164
Allen Gueco <allen.gueco1@gmail.com>
2024-11-01 16:38:06 -0400

    Update home/.config/nvim/lua/allengueco/plugins/rustacean.lua

--- home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
@@ -1,5 +1,32 @@
 return {
 	"mrcjkb/rustaceanvim",
 	version = "^5",
-	lazy = false,
+    ft = { 'rust' },
+    opts = {
+        server = {
+            on_attach = function(_, bufnr)
+                vim.keymap.set("n", "<F4>", function ()
+                    vim.cmd.RustLsp("codeAction")
+                end, { desc = "RustLsp Code Action", buffer = bufnr })
+
+                vim.keymap.set("n", "K", function ()
+                    vim.cmd.RustLsp({'hover', 'actions'})
+                end, { desc = "RustLsp Code Action", buffer = bufnr })
+            end,
+            default_settings = {
+                ['rust_analyzer'] = {
+                    cargo = {
+                        allFeatures = true,
+                    },
+                    checkOnSave = true,
+                    procMacro = {
+                        enable = true,
+                    }
+                }
+            }
+        }
+    },
+    config = function (_, opts)
+        vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim  or {}, opts or {})
+    end
 }
