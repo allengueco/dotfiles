90f2759148654ae7339a27fa5eb2bc9319dced28
Allen Gueco <allen.gueco1@gmail.com>
2024-11-01 15:27:52 -0400

    Update home/.config/nvim/lua/allengueco/plugins/rustacean.lua

--- home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/rustacean.lua
@@ -2,15 +2,27 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^5",
 	lazy = false,
-    config = function ()
-        vim.g.rustaceanvim = {
-            server = {
-                capabilities = require('lsp-zero').get_capabilities()
-            }
-        }
-    end,
-    keys = {
-        { "K", function () vim.cmd.RustLsp({'hover', 'action'}) end, silent = true },
-        { "<F4>", function () vim.cmd.RustLsp('codeAction') end, silent = true }
-    }
+	config = function()
+		vim.g.rustaceanvim = {
+			server = {
+				capabilities = require("lsp-zero").get_capabilities(),
+			},
+		}
+	end,
+	keys = {
+		{
+			"K",
+			function()
+				vim.cmd.RustLsp({ "hover", "action" })
+			end,
+			silent = true,
+		},
+		{
+			"<F4>",
+			function()
+				vim.cmd.RustLsp("codeAction")
+			end,
+			silent = true,
+		},
+	},
 }
