52ae8a654d1404e5261989cadcd900c9f4563945
Allen Gueco <allen.gueco1@gmail.com>
2024-11-06 02:24:48 -0500

    Update home/.config/nvim/lua/allengueco/lazy.lua

--- home/dot_config/nvim/lua/allengueco/lazy.lua
+++ home/dot_config/nvim/lua/allengueco/lazy.lua
@@ -33,7 +33,9 @@ require("lazy").setup({
 		{
 			"m4xshen/hardtime.nvim",
 			dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-			opts = {},
+			opts = {
+				disabled_filetypes = { "grug-far" },
+			},
 		},
 		{ "nvim-treesitter/nvim-treesitter-context" },
 		{ "nvim-tree/nvim-web-devicons" },
