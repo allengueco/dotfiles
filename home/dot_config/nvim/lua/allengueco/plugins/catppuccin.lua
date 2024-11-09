127b895fbaab468b939f4f103ff04988d6d8ad43
Allen Gueco <allen.gueco1@gmail.com>
2024-11-09 03:21:48 -0500

    Update home/.config/nvim/lua/allengueco/plugins/catppuccin.lua

--- home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
@@ -10,6 +10,7 @@ return {
                     nvim_surround = true,
                     blink_cmp = true,
                     lsp_trouble = true,
+                    gitsigns = true,
                     diffview = true,
                     mason = true,
                     noice = true,
