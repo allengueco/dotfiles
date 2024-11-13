97518b4048204eeb4e28b02e9bf96517b1bb41e1
Allen Gueco <allen.gueco1@gmail.com>
2024-11-13 14:00:21 -0500

    Update home/.config/nvim/lua/allengueco/plugins/java.lua

--- home/dot_config/nvim/lua/allengueco/plugins/java.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/java.lua
@@ -1,14 +1,14 @@
 return {
 	"nvim-java/nvim-java",
 	config = function()
-        require("java").setup({
-            jdk = {
-                auto_install = false,
-            },
-            notifications = {
-                dap = false,
-            },
-        })
+		require("java").setup({
+			jdk = {
+				auto_install = false,
+			},
+			notifications = {
+				dap = false,
+			},
+		})
 		require("lspconfig").jdtls.setup({
 			handlers = {
 				["$/progress"] = function(_, result, ctx) end,
