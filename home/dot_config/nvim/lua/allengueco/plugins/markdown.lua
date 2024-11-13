3d64d250bf85e22efbc5ff21c457119a7eab4ac2
Allen Gueco <allen.gueco1@gmail.com>
2024-11-13 16:13:09 -0500

    Update home/.config/nvim/lua/allengueco/plugins/markdown.lua

--- home/dot_config/nvim/lua/allengueco/plugins/markdown.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/markdown.lua
@@ -1,16 +1,27 @@
 return {
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {
-            preset = 'obsidian',
-            anti_conceal = {
-                ignore = {
-                    link = true,
-                }
-            }
-        },
-    }
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {
+			preset = "obsidian",
+			overrides = {
+				link = {
+					custom = {
+						jdt = {
+							pattern = "^jdt://",
+							icon = "󰖟 ",
+							highlight = "RenderMarkdownLink",
+						},
+					},
+				},
+				anti_conceal = {
+					ignore = {
+						link = true,
+					},
+				},
+			},
+		},
+	},
 }
