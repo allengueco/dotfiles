6bf23d297dc0ba941748efe5b449c8ce7e184e14
Allen Gueco <allen.gueco1@gmail.com>
2024-11-22 01:50:15 -0500

    Update home/.config/nvim/lua/allengueco/plugins/chezmoi.lua

--- home/dot_config/nvim/lua/allengueco/plugins/chezmoi.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/chezmoi.lua
@@ -1,21 +1,34 @@
 return {
-    {
-        'alker0/chezmoi.vim',
-        lazy = false,
-        init = function()
-            vim.g['chezmoi#use_tmp_buffer'] = true
-        end
-    },
-    {
-        'xvzc/chezmoi.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {
-            edit = {
-                watch = true,
-            },
-            notification = {
-                on_watch = true,
-            }
-        },
-    }
+	{
+		"alker0/chezmoi.vim",
+		lazy = false,
+		init = function()
+			vim.g["chezmoi#use_tmp_buffer"] = true
+		end,
+	},
+	{
+		"xvzc/chezmoi.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			edit = {
+				watch = true,
+			},
+			notification = {
+				on_watch = true,
+			},
+		},
+		init = function()
+			--  e.g. ~/.local/share/chezmoi/*
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = { os.getenv("HOME") or os.getenv("UserProfile") .. "/.local/share/chezmoi/*" },
+				callback = function(ev)
+					local bufnr = ev.buf
+					local edit_watch = function()
+						require("chezmoi.commands.__edit").watch(bufnr)
+					end
+					vim.schedule(edit_watch)
+				end,
+			})
+		end,
+	},
 }
