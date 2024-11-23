38717dd6e84796a64e64e7f2627dde6e26e8c4e2
Allen Gueco <allen.gueco1@gmail.com>
2024-11-23 06:42:59 -0500

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -34,33 +34,14 @@ return {
 			},
 			windows = {
 				autocomplete = {
-					selection = "manual",
+					selection = "auto_insert",
 				},
 				documentation = {
 					auto_show = true,
 				},
 			},
 			nerd_font_variant = "mono",
-			keymap = {
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "hide" },
-				["<Tab>"] = {
-					function(cmp)
-						if cmp.is_in_snippet() then
-							return cmp.accept()
-						else
-							return cmp.select_and_accept()
-						end
-					end,
-					"snippet_forward",
-					"fallback",
-				},
-				["<S-Tab>"] = { "snippet_backward", "fallback" },
-				["<C-p>"] = { "select_prev", "fallback" },
-				["<C-n>"] = { "select_next", "fallback" },
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-			},
+			keymap = "enter",
 			accept = {
 				auto_brackets = {
 					enabled = true,
