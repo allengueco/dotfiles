063c928f2f22efb951c00b47599405971d6389cd
Allen Gueco <allen.gueco1@gmail.com>
2024-11-24 03:30:16 -0500

    Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/allengueco/plugins/catppuccin.lua

--- home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/catppuccin.lua
@@ -1,42 +1,41 @@
 return {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-        require('catppuccin').setup(
-            {
-                integrations = {
-                    nvim_surround = true,
-                    blink_cmp = true,
-                    lsp_trouble = true,
-                    gitsigns = true,
-                    diffview = true,
-                    mason = true,
-                    noice = true,
-                    notify = true,
-                    indent_blankline = {
-                        enabled = true,
-                        scope_color = "lavender",
-                        colored_indent_levels = false,
-                    },
-                    neotest = true,
-                    native_lsp = {
-                        enabled = true,
-                        underlines = {
-                            errors = { "undercurl" },
-                            hints = { "undercurl" },
-                            warnings = { "undercurl" },
-                            information = { "undercurl" },
-                        },
-                    },
-                    which_key = true,
-                },
-                styles = {
-                    functions = { "italic" },
-                    types = { "italic" }
-                }
-            }
-        )
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				nvim_surround = true,
+				blink_cmp = true,
+				lsp_trouble = true,
+				gitsigns = true,
+				diffview = true,
+				mason = true,
+				noice = true,
+				notify = true,
+				snacks = true,
+				indent_blankline = {
+					enabled = true,
+					scope_color = "lavender",
+					colored_indent_levels = false,
+				},
+				neotest = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				which_key = true,
+			},
+			styles = {
+				functions = { "italic" },
+				types = { "italic" },
+			},
+		})
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
