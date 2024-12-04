076866a9f279da865760025ac837e3b68e749400
Allen Gueco <allen.gueco1@gmail.com>
2024-12-04 17:25:16 -0500

    Update home/.config/nvim/lua/allengueco/remap.lua

--- home/dot_config/nvim/lua/allengueco/remap.lua
+++ home/dot_config/nvim/lua/allengueco/remap.lua
@@ -4,6 +4,3 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
