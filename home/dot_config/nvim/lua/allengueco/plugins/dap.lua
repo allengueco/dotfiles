c30f4208683efca8458911c4dc39977cf0694ac6
Allen Gueco <allen.gueco1@gmail.com>
2024-12-19 17:33:02 -0500

    Update home/.config/nvim/lua/allengueco/plugins/dap.lua

--- home/dot_config/nvim/lua/allengueco/plugins/dap.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/dap.lua
@@ -1,7 +1,7 @@
 return {
 	"mfussenegger/nvim-dap",
 	keys = {
-		{ "<F5>", "<Cmd>lua require('dap').continue()<CR>" },
+		{ "<F5>", "<Cmd>lua require('dap').continue()<CR>", desc = "Continue" },
 		{ "<F7>", "<Cmd>lua require('dap').step_into()<CR>", desc = "Step into" },
 		{ "<F8>", "<Cmd>lua require('dap').step_over()<CR>", desc = "Step over" },
 		{ "<F9>", "<Cmd>lua require('dap').step_out()<CR>", desc = "Step out" },
@@ -10,24 +10,28 @@ return {
 			function()
 				require("dap").toggle_breakpoint()
 			end,
+            desc = "(DAP) Toggle breakpoint"
 		},
 		{
 			"<Leader>B",
 			function()
 				require("dap").set_breakpoint()
 			end,
+            desc = "(DAP) Set breakpoint"
 		},
 		{
 			"<Leader>dh",
 			function()
 				require("dap.ui.widgets").hover()
 			end,
+            desc = "(DAP) Hover"
 		},
 		{
 			"<Leader>dp",
 			function()
 				require("dap.ui.widgets").preview()
 			end,
+            desc = "(DAP) Preview"
 		},
 		{
 			"<Leader>dF",
@@ -35,6 +39,7 @@ return {
 				local widgets = require("dap.ui.widgets")
 				widgets.centered_float(widgets.frames)
 			end,
+            desc = "(DAP) Float"
 		},
 		{
 			"<Leader>dS",
@@ -42,6 +47,7 @@ return {
 				local widgets = require("dap.ui.widgets")
 				widgets.centered_float(widgets.scopes)
 			end,
+            desc = "(DAP) Scope"
 		},
 	},
 }
