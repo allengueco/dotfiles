87545532cde0ffab4c29c412a126ef729e4db371
Allen Gueco <allen.gueco1@gmail.com>
2024-12-19 05:40:42 -0500

    Update home/.config/nvim/lua/allengueco/plugins/leetcode.lua

--- home/dot_config/nvim/lua/allengueco/plugins/leetcode.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/leetcode.lua
@@ -1,35 +1,40 @@
 local leet_arg = "leetcode.nvim"
 
 return {
-    'kawre/leetcode.nvim',
-    build = ':TSUpdate html',
-    lazy = leet_arg ~= vim.fn.argv()[1],
-    dependencies = {
-        'nvim-telescope/telescope.nvim',
-        'nvim-lua/plenary.nvim',
-        'MunifTanjim/nui.nvim',
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-tree/nvim-web-devicons',
-    },
-    opts = {
-        lang = 'java',
-        arg = leet_arg,
-    },
-    keys = {
-        {
-            '<leader>ll', '<Cmd>Leet list<CR>'
-        },
-        {
-            '<leader>lt', '<Cmd>Leet test<CR>'
-        },
-        {
-            '<leader>ld', '<Cmd>Leet desc<CR>'
-        },
-        {
-            '<leader>li', '<Cmd>Leet info<CR>'
-        },
-        {
-            '<leader>lc', '<Cmd>Leet console<CR>'
-        }
-    }
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html",
+	lazy = leet_arg ~= vim.fn.argv()[1],
+	dependencies = {
+		"ibhagwan/fzf-lua",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		lang = "java",
+		arg = leet_arg,
+	},
+	keys = {
+		{
+			"<leader>ll",
+			"<Cmd>Leet list<CR>",
+		},
+		{
+			"<leader>lt",
+			"<Cmd>Leet test<CR>",
+		},
+		{
+			"<leader>ld",
+			"<Cmd>Leet desc<CR>",
+		},
+		{
+			"<leader>li",
+			"<Cmd>Leet info<CR>",
+		},
+		{
+			"<leader>lc",
+			"<Cmd>Leet console<CR>",
+		},
+	},
 }
