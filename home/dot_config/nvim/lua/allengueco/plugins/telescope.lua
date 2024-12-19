81d055916b26f61439b7e889c9cae0716ad1cea4
Allen Gueco <allen.gueco1@gmail.com>
2024-12-19 05:30:47 -0500

    Update home/.config/nvim/lua/allengueco/plugins/telescope.lua

--- home/dot_config/nvim/lua/allengueco/plugins/telescope.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	lazy = false,
+    enabled = false,
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope-ui-select.nvim" },
