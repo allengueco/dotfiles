0a6ee8cfc156e6b7a5c950628174dc9c41b54568
Allen Gueco <allen.gueco1@gmail.com>
2024-12-21 18:53:14 -0500

    Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/allengueco/plugins/treesj.lua

--- home/dot_config/nvim/lua/allengueco/plugins/treesj.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/treesj.lua
@@ -3,20 +3,8 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	opts = {},
 	keys = {
-		{
-			"<leader>m",
-			"<Cmd>TSJToggle<CR>",
-			desc = "Toggle Node under curser (TSJ)",
-		},
-		{
-			"<leader>s",
-			"<Cmd>TSJSplit<CR>",
-			desc = "Split Node under curser (TSJ)",
-		},
-		{
-			"<leader>j",
-			"<Cmd>TSJJoin<CR>",
-			desc = "Join Node under curser (TSJ)",
-		},
+		{ "<leader>m", "<Cmd>TSJToggle<CR>" },
+		{ "<leader>s", "<Cmd>TSJSplit<CR>" },
+		{ "<leader>j", "<Cmd>TSJJoin<CR>" },
 	},
 }
