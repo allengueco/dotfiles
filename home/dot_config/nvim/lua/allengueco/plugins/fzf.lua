4d3d47cd19690c477cc49520d4a85f16a6923a19
Allen Gueco <allen.gueco1@gmail.com>
2024-12-21 22:11:22 -0500

    Update home/.config/nvim/lua/allengueco/plugins/fzf.lua

--- home/dot_config/nvim/lua/allengueco/plugins/fzf.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/fzf.lua
@@ -3,5 +3,8 @@ return {
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {},
+		keys = {
+			{ "<leader>ff", "<Cmd>FzfLua files<CR>", desc = "Fzf Files" },
+		},
 	},
 }
