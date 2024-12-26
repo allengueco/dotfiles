14961c2fe19f51654e4451c247b6ca322161ec66
Allen Gueco <allen.gueco1@gmail.com>
2024-12-25 21:52:00 -0500

    Update home/.config/nvim/lua/allengueco/plugins/edgy.lua

--- home/dot_config/nvim/lua/allengueco/plugins/edgy.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/edgy.lua
@@ -1,74 +1,76 @@
 return {
-	"folke/edgy.nvim",
-	init = function()
-		vim.opt.laststatus = 3
-		vim.opt.splitkeep = "screen"
-	end,
-	opts = {
-		animate = { enabled = false },
-		left = {
-			{
-				title = "Neo-Tree",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "filesystem"
-				end,
-				size = { height = 0.5 },
-			},
-			{
-				title = "Neo-Tree Git",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "git_status"
-				end,
-				pinned = true,
-				collapsed = true, -- show window as closed/collapsed on start
-				open = "Neotree position=right git_status",
-			},
-			{
-				title = "Neo-Tree Buffers",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "buffers"
-				end,
-				pinned = true,
-				collapsed = true, -- show window as closed/collapsed on start
-				open = "Neotree position=top buffers",
-			},
-			"neo-tree",
-		},
-		bottom = {
-			{
-				ft = "snacks_terminal",
-				size = { height = 0.4 },
-				title = "%{b:snacks_terminal.id}: %{b:term_title}",
-				filter = function(_buf, win)
-					return vim.w[win].snacks_win
-						and vim.w[win].snacks_win.position == pos
-						and vim.w[win].snacks_win.relative == "editor"
-						and not vim.w[win].trouble_preview
-				end,
-			},
-			"Trouble",
-			{ ft = "qf", title = "QuickFix" },
-			{
-				ft = "help",
-				size = { height = 20 },
-				filter = function(buf)
-					return vim.bo[buf].buftype == "help"
-				end,
-			},
-		},
-		right = {
-			{
-				title = function()
-					local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
-					return vim.fn.fnamemodify(buf_name, ":t")
-				end,
-				ft = "Outline",
-				pinned = true,
-				open = "SymbolsOutlineOpen",
-			},
-		},
-	},
+    "folke/edgy.nvim",
+    init = function()
+        vim.opt.laststatus = 3
+        vim.opt.splitkeep = "screen"
+    end,
+    opts = function(_, opts)
+        opts.animate = { enabled = false }
+        opts.left = {
+            {
+                title = "Neo-Tree",
+                ft = "neo-tree",
+                filter = function(buf)
+                    return vim.b[buf].neo_tree_source == "filesystem"
+                end,
+                size = { height = 0.5 },
+            },
+            {
+                title = "Neo-Tree Git",
+                ft = "neo-tree",
+                filter = function(buf)
+                    return vim.b[buf].neo_tree_source == "git_status"
+                end,
+                pinned = true,
+                open = "Neotree position=right git_status",
+            },
+            {
+                title = "Neo-Tree Buffers",
+                ft = "neo-tree",
+                filter = function(buf)
+                    return vim.b[buf].neo_tree_source == "buffers"
+                end,
+                pinned = true,
+                open = "Neotree position=top buffers",
+            },
+            "neo-tree",
+        }
+        opts.bottom = {
+            "Trouble",
+            { ft = "qf", title = "QuickFix" },
+            {
+                ft = "help",
+                size = { height = 20 },
+                filter = function(buf)
+                    return vim.bo[buf].buftype == "help"
+                end,
+            },
+        }
+        opts.right = {
+            {
+                title = function()
+                    local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
+                    return vim.fn.fnamemodify(buf_name, ":t")
+                end,
+                ft = "Outline",
+                pinned = true,
+                open = "SymbolsOutlineOpen",
+            },
+        }
+
+        for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+            opts[pos] = opts[pos] or {}
+            table.insert(opts[pos], {
+                ft = "snacks_terminal",
+                size = { height = 0.4 },
+                title = "%{b:snacks_terminal.id}: %{b:term_title}",
+                filter = function(_, win)
+                    return vim.w[win].snacks_win
+                        and vim.w[win].snacks_win.position == pos
+                        and vim.w[win].snacks_win.relative == "editor"
+                        and not vim.w[win].trouble_preview
+                end,
+            })
+        end
+    end,
 }
