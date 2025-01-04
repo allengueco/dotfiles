3d48699e2f94eb6d94f72ae1463c1b5563d4eab4
Allen Gueco <allen.gueco1@gmail.com>
2025-01-03 19:39:43 -0500

    Update home/.config/nvim/lua/allengueco/plugins/bufferline.lua

--- home/dot_config/nvim/lua/allengueco/plugins/bufferline.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/bufferline.lua
@@ -6,6 +6,32 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	opts = function(_, opts)
+        -- Fix bufferline.nvim offsets https://github.com/folke/edgy.nvim/discussions/3
+		local Offset = require("bufferline.offset")
+		if not Offset.edgy then
+			local get = Offset.get
+			Offset.get = function()
+				if package.loaded.edgy then
+					local layout = require("edgy.config").layout
+					local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+					for _, pos in ipairs({ "left", "right" }) do
+						local sb = layout[pos]
+						if sb and #sb.wins > 0 then
+							local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+							ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+							ret[pos .. "_size"] = sb.bounds.width
+						end
+					end
+					ret.total_size = ret.left_size + ret.right_size
+					if ret.total_size > 0 then
+						return ret
+					end
+				end
+				return get()
+			end
+			Offset.edgy = true
+		end
+
 		if (vim.g.colors_name or ""):find("catppuccin") then
 			opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
 		end
