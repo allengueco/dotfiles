2ee8f867d2792f6600d7aa0eee15086fec46d089
Allen Gueco <allen.gueco1@gmail.com>
2025-01-30 04:11:12 -0500

    Update home/.config/nvim/lua/allengueco/plugins/haskell.lua

--- home/dot_config/nvim/lua/allengueco/plugins/haskell.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/haskell.lua
@@ -1,11 +1,5 @@
 return {
 	"mrcjkb/haskell-tools.nvim",
 	version = "^4",
-	lazy = false,
-	keys = {
-		{
-			"<leader>cl",
-			vim.lsp.codelens.run,
-		},
-	},
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
 }
