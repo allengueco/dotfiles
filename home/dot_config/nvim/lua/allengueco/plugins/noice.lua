ec23526e8add11609845e2d7f9c2b27a742f882f
Allen Gueco <allen.gueco1@gmail.com>
2025-03-02 04:54:44 -0500

    Update home/.config/nvim/lua/allengueco/plugins/noice.lua

--- home/dot_config/nvim/lua/allengueco/plugins/noice.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/noice.lua
@@ -1,44 +1,33 @@
 return {
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = { "MunifTanjim/nui.nvim", },
-        opts = {
-            cmdline = {
-                enabled = true,
-            },
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                }
-            },
-            presets = {
-                long_message_to_split = true,
-                command_palette = true,
-                inc_rename = false,
-                lsp_doc_border = false,
-            },
-            routes = {
-                {
-                    filter = {
-                        event = "lsp",
-                        kind = "progress",
-                        cond = function(message)
-                            local client = vim.tbl_get(message.opts, "progress", "client")
-                            return client == "jdtls"
-                        end,
-                    },
-                    opts = { skip = true },
-                },
-            },
+
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", },
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            }
         },
-    },
-    {
-        "rcarriga/nvim-notify",
-        enabled = false,
-        opts = {
-            render = "wrapped-compact",
+        presets = {
+            long_message_to_split = true,
+            command_palette = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+        },
+        routes = {
+            {
+                filter = {
+                    event = "lsp",
+                    kind = "progress",
+                    cond = function(message)
+                        local client = vim.tbl_get(message.opts, "progress", "client")
+                        return client == "jdtls"
+                    end,
+                },
+                opts = { skip = true },
+            },
         },
     },
 }
