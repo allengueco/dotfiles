cc33983aff34920ebb934ef1bfc12bffd02e9a7d
Allen Gueco <allen.gueco1@gmail.com>
2025-03-02 03:18:24 -0500

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.use_fancy_tab_bar = true
@@ -9,8 +9,9 @@ config.switch_to_last_active_tab_when_closing_tab = true
 config.color_scheme = "Catppuccin Mocha"
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback({
-    { family = 'JetBrainsMono Nerd Font', weight = "Medium" },
-    'Flog Symbols' })
+	{ family = "JetBrainsMono Nerd Font", weight = "Medium" },
+	"Flog Symbols",
+})
 config.window_decorations = "RESIZE"
 config.max_fps = 144
 config.initial_rows = 36
@@ -18,227 +19,224 @@ config.initial_cols = 96
 config.audible_bell = "Disabled"
 
 --- Windows Config
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_domain = 'WSL:Arch'
-    config.cell_width = 0.9
-    config.default_prog = { 'pwsh.exe', '-nologo' }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Arch"
+	config.cell_width = 0.9
+	config.default_prog = { "pwsh.exe", "-nologo" }
 end
 
 config.colors = {
-    tab_bar = {
-        -- The color of the strip that goes along the top of the window
-        -- (does not apply when fancy tab bar is in use)
-        background = '#0b0022',
+	tab_bar = {
+		-- The color of the strip that goes along the top of the window
+		-- (does not apply when fancy tab bar is in use)
+		background = "#0b0022",
 
-        -- The active tab is the one that has focus in the window
-        active_tab = {
-            -- The color of the background area for the tab
-            bg_color = '#2b2042',
-            -- The color of the text for the tab
-            fg_color = '#c0c0c0',
+		-- The active tab is the one that has focus in the window
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#2b2042",
+			-- The color of the text for the tab
+			fg_color = "#c0c0c0",
 
-            -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-            -- label shown for this tab.
-            -- The default is "Normal"
-            intensity = 'Normal',
+			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+			-- label shown for this tab.
+			-- The default is "Normal"
+			intensity = "Normal",
 
-            -- Specify whether you want "None", "Single" or "Double" underline for
-            -- label shown for this tab.
-            -- The default is "None"
-            underline = 'None',
+			-- Specify whether you want "None", "Single" or "Double" underline for
+			-- label shown for this tab.
+			-- The default is "None"
+			underline = "None",
 
-            -- Specify whether you want the text to be italic (true) or not (false)
-            -- for this tab.  The default is false.
-            italic = false,
+			-- Specify whether you want the text to be italic (true) or not (false)
+			-- for this tab.  The default is false.
+			italic = false,
 
-            -- Specify whether you want the text to be rendered with strikethrough (true)
-            -- or not for this tab.  The default is false.
-            strikethrough = false,
-        },
+			-- Specify whether you want the text to be rendered with strikethrough (true)
+			-- or not for this tab.  The default is false.
+			strikethrough = false,
+		},
 
-        -- Inactive tabs are the tabs that do not have focus
-        inactive_tab = {
-            bg_color = '#1b1032',
-            fg_color = '#808080',
+		-- Inactive tabs are the tabs that do not have focus
+		inactive_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
 
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `inactive_tab`.
-        },
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab`.
+		},
 
-        -- You can configure some alternate styling when the mouse pointer
-        -- moves over inactive tabs
-        inactive_tab_hover = {
-            bg_color = '#3b3052',
-            fg_color = '#909090',
-            italic = true,
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over inactive tabs
+		inactive_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
 
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `inactive_tab_hover`.
-        },
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab_hover`.
+		},
 
-        -- The new tab button that let you create new tabs
-        new_tab = {
-            bg_color = '#1b1032',
-            fg_color = '#808080',
+		-- The new tab button that let you create new tabs
+		new_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
 
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `new_tab`.
-        },
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab`.
+		},
 
-        -- You can configure some alternate styling when the mouse pointer
-        -- moves over the new tab button
-        new_tab_hover = {
-            bg_color = '#3b3052',
-            fg_color = '#909090',
-            italic = true,
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over the new tab button
+		new_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
 
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `new_tab_hover`.
-        },
-    },
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab_hover`.
+		},
+	},
 }
 
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+	left = "1cell",
+	right = "1cell",
+	top = "0.5cell",
+	bottom = "0.5cell",
 }
 
 config.leader = {
-    key = 'a',
-    mods = 'CTRL',
-    timeout_milliseconds = 2000,
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
 }
 -- smart-splits.nvim integration because not lazy loading
 local function is_vim(pane)
-    return pane:get_user_vars().IS_NVIM == 'true'
+	return pane:get_user_vars().IS_NVIM == "true"
 end
 local direction_keys = {
-    h = 'Left',
-    j = 'Down',
-    k = 'Up',
-    l = 'Right',
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
 
 local function split_nav(resize_or_move, key)
-    return {
-        key = key,
-        mods = resize_or_move == 'resize' and 'ALT' or 'CTRL',
-        action = wezterm.action_callback(function(win, pane)
-            if is_vim(pane) then
-                -- pass the keys through to nvim
-                win:perform_action({
-                    SendKey = { key = key, mods = resize_or_move == 'resize' and 'ALT' or 'CTRL' },
-                }, pane)
-            else
-                if resize_or_move == 'resize' then
-                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-                else
-                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-                end
-            end
-        end),
-    }
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "ALT" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "ALT" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
 end
 config.keys = {
-    -- move between split panes
-    split_nav('move', 'h'),
-    split_nav('move', 'j'),
-    split_nav('move', 'k'),
-    split_nav('move', 'l'),
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
 
-    -- resize panes
-    split_nav('resize', 'h'),
-    split_nav('resize', 'j'),
-    split_nav('resize', 'k'),
-    split_nav('resize', 'l'),
-    {
-        key = '[',
-        mods = 'LEADER',
-        action = wezterm.action.ActivateCopyMode,
-    },
-    {
-        key = 'c',
-        mods = 'LEADER',
-        action = wezterm.action.SpawnTab 'CurrentPaneDomain'
-    },
-    {
-        key = 'z',
-        mods = 'ALT',
-        action = wezterm.action.TogglePaneZoomState,
-    },
-    {
-        key = 'n',
-        mods = 'LEADER',
-        action = wezterm.action.ActivateTabRelative(1),
-    },
-    {
-        key = 'p',
-        mods = 'LEADER',
-        action = wezterm.action.ActivateTabRelative(-1),
-    },
-    {
-        key = ',',
-        mods = 'LEADER',
-        action = wezterm.action.PromptInputLine {
-            description = 'Enter new name for tab',
-            action = wezterm.action_callback(
-                function(window, pane, line)
-                    if line then
-                        window:active_tab():set_title(line)
-                    end
-                end
-            ),
-        },
-    },
-    {
-        key = 'w',
-        mods = 'LEADER',
-        action = wezterm.action.ShowTabNavigator,
-    },
-    {
-        key = '&',
-        mods = 'LEADER|SHIFT',
-        action = wezterm.action.CloseCurrentTab { confirm = true },
-    },
-    {
-        key = '|',
-        mods = 'LEADER|SHIFT',
-        action = wezterm.action.SplitPane {
-            direction = 'Right',
-            size = { Percent = 50 }
-        }
-    },
-    {
-        key = '-',
-        mods = 'LEADER',
-        action = wezterm.action.SplitPane {
-            direction = 'Down',
-            size = { Percent = 50 }
-        }
-    },
-    {
-        key = '{',
-        mods = 'LEADER|SHIFT',
-        action = wezterm.action.PaneSelect { mode = 'SwapWithActiveKeepFocus' }
-    },
-    {
-        key = ';',
-        mods = 'LEADER',
-        action = wezterm.action.ActivatePaneDirection('Prev'),
-    },
-    {
-        key = 'o',
-        mods = 'LEADER',
-        action = wezterm.action.ActivatePaneDirection('Next'),
-    },
-    {
-        key = 'l',
-        mods = 'LEADER',
-        action = wezterm.action.ShowLauncher
-    },
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "z",
+		mods = "ALT",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = ",",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.ShowTabNavigator,
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "{",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+	},
+	{
+		key = ";",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ShowLauncher,
+	},
 }
-
 
 return config
