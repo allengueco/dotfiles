6ad9d7ca36a7e72df201320183c1a330d268539a
Allen Gueco <allen.gueco1@gmail.com>
2025-03-02 02:24:45 -0500

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
 local config = wezterm.config_builder()
-
 
 config.use_fancy_tab_bar = true
 config.tab_max_width = 32
@@ -8,6 +7,7 @@ config.pane_focus_follows_mouse = true
 config.scrollback_lines = 5000
 config.switch_to_last_active_tab_when_closing_tab = true
 config.color_scheme = "Catppuccin Mocha"
+config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback({
     { family = 'JetBrainsMono Nerd Font', weight = "Medium" },
     'Flog Symbols' })
