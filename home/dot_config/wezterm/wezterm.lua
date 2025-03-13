ded0ebef6defeb34ea0238268d41c3a55ca59f4e
Allen Gueco <allen.gueco1@gmail.com>
2025-03-13 01:07:04 -0400

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -3,41 +3,9 @@ local config = wezterm.config_builder()
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
 tabline.setup({
-  options = {
-    icons_enabled = true,
-    theme = 'Catppuccin Mocha',
-    tabs_enabled = true,
-    theme_overrides = {},
-    section_separators = {
-      left = wezterm.nerdfonts.pl_left_hard_divider,
-      right = wezterm.nerdfonts.pl_right_hard_divider,
-    },
-    component_separators = {
-      left = wezterm.nerdfonts.pl_left_soft_divider,
-      right = wezterm.nerdfonts.pl_right_soft_divider,
-    },
-    tab_separators = {
-      left = wezterm.nerdfonts.pl_left_hard_divider,
-      right = wezterm.nerdfonts.pl_right_hard_divider,
-    },
-  },
-  sections = {
-    tabline_a = { 'mode' },
-    tabline_b = { 'workspace' },
-    tabline_c = { ' ' },
-    tab_active = {
-      'index',
-      { 'parent', padding = 0 },
-      '/',
-      { 'cwd', padding = { left = 0, right = 1 } },
-      { 'zoomed', padding = 0 },
-    },
-    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
-    tabline_x = { 'ram', 'cpu' },
-    tabline_y = { 'datetime', 'battery' },
-    tabline_z = { 'domain' },
-  },
-  extensions = {},
+	options = {
+		theme = "Catppuccin Mocha",
+	},
 })
 
 config.scrollback_lines = 5000
@@ -45,9 +13,9 @@ config.switch_to_last_active_tab_when_closing_tab = true
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({
 	{
-        family = "JetBrainsMono Nerd Font",
-        weight = "Medium",
-    },
+		family = "JetBrainsMono Nerd Font",
+		weight = "Medium",
+	},
 	"Flog Symbols",
 })
 config.window_decorations = "RESIZE"
