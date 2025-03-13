9ed2ad73a5398250000774b41439ce3ea1d59cdb
Allen Gueco <allen.gueco1@gmail.com>
2025-03-13 00:57:06 -0400

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,5 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+    options = {
+        icons_enabled = true,
+        theme = 'Catppuccin Mocha',
+    }
+})
 
 config.use_fancy_tab_bar = true
 config.tab_max_width = 32
