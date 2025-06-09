cfd50a2702d1f6393633ed68fa96669f584578e6
Allen Gueco <allen.gueco1@gmail.com>
2025-06-09 01:36:55 -0400

    Update home/.config/nvim/lua/allengueco/plugins/lsp.lua

--- home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/lsp.lua
@@ -88,6 +88,10 @@ return {
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig"
+        },
 		opts = {
             automatic_enable = {
                 exclude = {
