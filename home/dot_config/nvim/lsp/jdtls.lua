66c7e657f501877fe8308ec6d3d132bed63b69b1
Allen Gueco <allen.gueco1@gmail.com>
2025-11-22 16:59:25 -0500

    Update .config/nvim/lsp/jdtls.lua

--- home/dot_config/nvim/lsp/jdtls.lua
+++ home/dot_config/nvim/lsp/jdtls.lua
@@ -42,28 +42,28 @@ local function get_jdtls_paths()
 
 	path.bundles = {}
 
-	---
-	-- Include java-test bundle if present
-	---
-	local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
-
-	local java_test_bundle = vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar"), "\n")
-
-	if java_test_bundle[1] ~= "" then
-		vim.list_extend(path.bundles, java_test_bundle)
-	end
-
-	---
-	-- Include java-debug-adapter bundle if present
-	---
-	local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
-
-	local java_debug_bundle =
-		vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n")
-
-	if java_debug_bundle[1] ~= "" then
-		vim.list_extend(path.bundles, java_debug_bundle)
-	end
+	-- ---
+	-- -- Include java-test bundle if present
+	-- ---
+	-- local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
+	--
+	-- local java_test_bundle = vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar"), "\n")
+	--
+	-- if java_test_bundle[1] ~= "" then
+	-- 	vim.list_extend(path.bundles, java_test_bundle)
+	-- end
+	--
+	-- ---
+	-- -- Include java-debug-adapter bundle if present
+	-- ---
+	-- local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+	--
+	-- local java_debug_bundle =
+	-- 	vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n")
+	--
+	-- if java_debug_bundle[1] ~= "" then
+	-- 	vim.list_extend(path.bundles, java_debug_bundle)
+	-- end
 
 	---
 	-- configure sts4
