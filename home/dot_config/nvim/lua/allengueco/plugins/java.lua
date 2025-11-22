0213fa6af7a504567ba90e22809bb980857c155a
Allen Gueco <allen.gueco1@gmail.com>
2025-11-22 17:35:17 -0500

    Update .config/nvim/lua/allengueco/plugins/java.lua

--- home/dot_config/nvim/lua/allengueco/plugins/java.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/java.lua
@@ -1,12 +1,3 @@
 return {
-	"nvim-java/nvim-java",
-	ft = "java",
-	opts = {
-		jdk = {
-			auto_install = false,
-		},
-		notifications = {
-			dap = false,
-		},
-	},
+    "mfussenegger/nvim-jdtls"
 }
