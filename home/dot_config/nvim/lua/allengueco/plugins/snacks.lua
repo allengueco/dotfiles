a9b49297125314a38c280cbe9f1990f63059bc12
Allen Gueco <allen.gueco1@gmail.com>
2025-11-22 21:26:24 -0500

    Update .config/nvim/lua/allengueco/plugins/snacks.lua

--- home/dot_config/nvim/lua/allengueco/plugins/snacks.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/snacks.lua
@@ -75,24 +75,24 @@ return {
 				{ section = "startup" },
 			},
 		},
-		init = function()
-			vim.api.nvim_create_autocmd("LspProgress", {
-				---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-				callback = function(ev)
-					local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-					vim.notify(vim.lsp.status(), "info", {
-						id = "lsp_progress",
-						title = "LSP Progress",
-						opts = function(notif)
-							notif.icon = ev.data.params.value.kind == "end" and " "
-								or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-						end,
-					})
-				end,
-			})
-		end,
 		styles = {},
 	},
+	init = function()
+		vim.api.nvim_create_autocmd("LspProgress", {
+			---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+			callback = function(ev)
+				local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+				vim.notify(vim.lsp.status(), "info", {
+					id = "lsp_progress",
+					title = "LSP Progress",
+					opts = function(notif)
+						notif.icon = ev.data.params.value.kind == "end" and " "
+							or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+					end,
+				})
+			end,
+		})
+	end,
     -- stylua: ignore
 	keys = {
 		{ "<leader>T",  function() Snacks.terminal.toggle() end, "Open Terminal" },
