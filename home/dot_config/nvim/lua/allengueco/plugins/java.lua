d5beba7b627429fb8ef18bc5dd9529858c6e17a5
Allen Gueco <allen.gueco1@gmail.com>
2025-11-22 19:31:37 -0500

    Update .config/nvim/lua/allengueco/plugins/java.lua

--- home/dot_config/nvim/lua/allengueco/plugins/java.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/java.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"mfussenegger/nvim-jdtls",
+		ft = { "java" },
 	},
 	-- Using autocmd launch (default)
 	-- Default uses jars from mason or ~/.vscode/extensions/vmware.vscode-spring-boot-x.x.x
