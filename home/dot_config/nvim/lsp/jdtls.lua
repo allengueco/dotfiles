130a8bb8e048a99f3ae5219f991aa3f66d712188
Allen Gueco <allen.gueco1@gmail.com>
2025-11-22 19:57:32 -0500

    Update .config/nvim/lsp/jdtls.lua

--- home/dot_config/nvim/lsp/jdtls.lua
+++ home/dot_config/nvim/lsp/jdtls.lua
@@ -1,12 +1,24 @@
 local java_cmds = vim.api.nvim_create_augroup("java_cmds", { clear = true })
 local cache_vars = {}
 
-local root_files = {
+local root_markers1 = {
+	-- Multi-module projects
+	"mvnw", -- Maven
+	"gradlew", -- Gradle
+	"settings.gradle", -- Gradle
+	"settings.gradle.kts", -- Gradle
+	-- Use git directory as last resort for multi-module maven projects
+	-- In multi-module maven projects it is not really possible to determine what is the parent directory
+	-- and what is submodule directory. And jdtls does not break if the parent directory is at higher level than
+	-- actual parent pom.xml so propagating all the way to root git directory is fine
 	".git",
-	"mvnw",
-	"gradlew",
-	"pom.xml",
-	"build.gradle",
+}
+local root_markers2 = {
+	-- Single-module projects
+	"build.xml", -- Ant
+	"pom.xml", -- Maven
+	"build.gradle", -- Gradle
+	"build.gradle.kts", -- Gradle
 }
 
 local features = {
@@ -229,8 +241,9 @@ local cmd = {
 
 return {
 	cmd = cmd,
-    on_attach = jdtls_on_attach,
-    root_markers = root_files,
+	on_attach = jdtls_on_attach,
+	root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers1, root_markers2 }
+		or vim.list_extend(root_markers1, root_markers2),
 	settings = lsp_settings,
 	init_options = {
 		bundles = path.bundles,
