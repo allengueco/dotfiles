e87c159d56e68976c9be4be92fb3e76a10bde6c7
Allen Gueco <allen.gueco1@gmail.com>
2026-02-01 16:53:51 -0500

    Update .config/nvim/lua/allengueco/plugins/ai.lua

--- home/dot_config/nvim/lua/allengueco/plugins/ai.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/ai.lua
@@ -9,15 +9,5 @@ return {
 				adapter = "anthropic",
 			},
 		},
-        adapters = {
-            http = {
-                anthropic = function()
-                    return require("codecompanion.adapters").extend("anthropic", {
-                        env = {
-                            api_key = ""
-                        }
-                    })
-            }
-        }
 	},
 }
