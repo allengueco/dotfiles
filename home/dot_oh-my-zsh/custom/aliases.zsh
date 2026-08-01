c1441249339754cff82b846a8693e86a08331bfb
Allen Gueco <allen.gueco1@gmail.com>
2026-08-01 04:50:12 -0400

    Update .oh-my-zsh/custom/aliases.zsh

--- home/dot_oh-my-zsh/custom/aliases.zsh
+++ home/dot_oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,8 @@
 alias cz="chezmoi"
 alias ls="eza --icons -F -H --group-directories-first --git -1"
 alias czu="chezmoi update"
-alias xdg-open="wsl-open"
+
+if [[ $(uname -r) =~ (m|M)icrosoft ]]; then
+    alias xdg-open="wsl-open"
+fi
+
