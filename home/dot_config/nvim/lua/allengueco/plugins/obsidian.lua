4d5a06f1be8337c192cce22b6a23226adc59ebba
Allen Gueco <allen.gueco1@gmail.com>
2026-08-01 02:58:18 -0400

    Update .config/nvim/lua/allengueco/plugins/obsidian.lua

--- home/dot_config/nvim/lua/allengueco/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/obsidian.lua
@@ -1,38 +1,32 @@
 return {
-    'epwalsh/obsidian.nvim',
+    'obsidian-nvim/obsidian.nvim',
     version = '*',
-    lazy = true,
-    event = {
-        'BufReadPre ' .. vim.fn.expand('~') .. '/vaults/**/*.md',
-        'BufNewFile ' .. vim.fn.expand('~') .. '/vaults/**/*.md'
-    },
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-    },
+    ---@module 'obsidian'
+    ---@type obsidian.config
     opts = {
-        mappings = {
-            -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-            ["gf"] = {
-                action = function()
-                    return require("obsidian").util.gf_passthrough()
-                end,
-                opts = { noremap = false, expr = true, buffer = true },
-            },
-            -- Toggle check-boxes.
-            ["<leader>ch"] = {
-                action = function()
-                    return require("obsidian").util.toggle_checkbox()
-                end,
-                opts = { buffer = true },
-            },
-            -- Smart action depending on context, either follow link or toggle checkbox.
-            ["<cr>"] = {
-                action = function()
-                    return require("obsidian").util.smart_action()
-                end,
-                opts = { buffer = true, expr = true },
-            }
-        },
+        -- mappings = {
+        --     -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        --     ["gf"] = {
+        --         action = function()
+        --             return require("obsidian").util.gf_passthrough()
+        --         end,
+        --         opts = { noremap = false, expr = true, buffer = true },
+        --     },
+        --     -- Toggle check-boxes.
+        --     ["<leader>ch"] = {
+        --         action = function()
+        --             return require("obsidian").util.toggle_checkbox()
+        --         end,
+        --         opts = { buffer = true },
+        --     },
+        --     -- Smart action depending on context, either follow link or toggle checkbox.
+        --     ["<cr>"] = {
+        --         action = function()
+        --             return require("obsidian").util.smart_action()
+        --         end,
+        --         opts = { buffer = true, expr = true },
+        --     }
+        -- },
         workspaces = {
             {
                 name = "aws",
