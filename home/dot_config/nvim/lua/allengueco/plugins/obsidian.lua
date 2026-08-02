e9c07f913068ccdf2858ee830b913654ea603f40
Allen Gueco <allen.gueco1@gmail.com>
2026-08-02 03:42:52 -0400

    Update .config/nvim/lua/allengueco/plugins/obsidian.lua

--- home/dot_config/nvim/lua/allengueco/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/allengueco/plugins/obsidian.lua
@@ -3,29 +3,31 @@ return {
 	version = "*",
 	---@module 'obsidian'
 	---@type obsidian.config
-	opts = {
-		legacy_commands = false,
-		picker = {
-			name = "snacks.picker",
-		},
-		workspaces = {
-			{ name = "personal", path = "~/vaults/personal" },
-			{ name = "work", path = "~/vaults/work" },
-		},
-        note_id_func = function()
-            return require("obsidian.builtin").title_id
-        end,
-        notes_subdir = "notes",
-        new_notes_location = "notes_subdir",
-        daily_notes = {
-            folder = "daily",
-            date_format = "%Y-%m-%d",
-        },
-        templates = {
-            substitutions = {},
-            folder = "templates",
-            date_format = "%Y-%m-%d",
-            time_format = "%HH:%M"
-        }
-	},
+	opts = function()
+		return {
+			legacy_commands = false,
+			picker = {
+				name = "snacks.picker",
+			},
+			workspaces = {
+				{ name = "personal", path = "~/vaults/personal" },
+				{ name = "work", path = "~/vaults/work" },
+			},
+			note_id_func = function()
+				return require("obsidian.builtin").title_id
+			end,
+			notes_subdir = "notes",
+			new_notes_location = "notes_subdir",
+			daily_notes = {
+				folder = "daily",
+				date_format = "%Y-%m-%d",
+			},
+			templates = {
+				substitutions = {},
+				folder = "templates",
+				date_format = "%Y-%m-%d",
+				time_format = "%HH:%M",
+			},
+		}
+	end,
 }
